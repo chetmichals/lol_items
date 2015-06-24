ba316c03ee098f33d8a1ad34d7458293462a83b7
Chet Michals <chetmichals@Gmail.com>
2015-06-24 16:27:30 -0500

    Created HP5 system, MR system, and several more items
    None of this has been tested yet, so here hoping it works!

--- game/scripts/vscripts/items/utility.lua
+++ game/scripts/vscripts/items/utility.lua
@@ -18,6 +18,8 @@ function RestoreMana(keys)
 end
 
 
+
+
 function AddCrit(keys)
 	playerHero = keys.caster:GetPlayerOwner():GetAssignedHero()
 	criticleHit = playerHero:FindModifierByName("modifier_basic_crtitical_strike_stack")
@@ -121,4 +123,61 @@ function ApplyCrit(keys)
 	crit_Buff:ApplyDataDrivenModifier(playerHero,playerHero,"modifier_lol_basic_crit",nil)
 	UTIL_RemoveImmediate(crit_Buff)
 	crit_Buff = nil
+end
+
+function AddMagicResistItem(keys)
+	hero = keys.caster:GetPlayerOwner():GetAssignedHero()
+	if hero.magicResistance == nil then
+		hero.magicResistance = 0
+	end
+	hero.magicResistance = hero.magicResistance + keys.magicResist
+	SetMagicResistance(hero)
+end
+
+function RemoveMagicResistItem(keys)
+	hero = keys.caster:GetPlayerOwner():GetAssignedHero()
+	if hero.magicResistance == nil then
+		hero.magicResistance = 100
+	end
+	hero.magicResistance = hero.magicResistance - keys.magicResist
+	SetMagicResistance(hero)
+end
+
+function SetMagicResistance(hero)
+	if newMagicResistance >= 0 then
+		newMagicResistance = 100 / (hero.magicResistance + 100)
+		newMagicResistance = (1 - newMagicResistance) * 100
+		hero:SetBaseMagicalResistanceValue(newMagicResistance)
+	elseif newMagicResistance >= -100 then
+		newMagicResistance = 100 / (100 - hero.magicResistance)
+		newMagicResistance = 2 - newMagicResistance
+		newMagicResistance = (1 - newMagicResistance) * 100
+		hero:SetBaseMagicalResistanceValue(newMagicResistance)
+	else
+		hero:SetBaseMagicalResistanceValue(-100)
+	end
+end
+
+function AddADItem(keys)
+	hero = keys.caster:GetPlayerOwner():GetAssignedHero()
+	damage = hero:GetBaseDamageMax()
+	damage = damage + keys.ad
+	hero:SetBaseDamageMin(damage)
+	hero:SetBaseDamageMax(damage)
+end
+
+function RemoveADItem(keys)
+	hero = keys.caster:GetPlayerOwner():GetAssignedHero()
+	damage = hero:GetBaseDamageMax()
+	damage = damage - keys.ad
+	hero:SetBaseDamageMin(damage)
+	hero:SetBaseDamageMax(damage)
+end
+
+function HP5Heal(keys)
+	hero = keys.caster:GetPlayerOwner():GetAssignedHero()
+	baseHeal = hero:GetBaseHealthRegen()
+	baseHeal = baseHeal * (1 + keys.healPercent)
+	baseHeal = baseHeal / 10
+	hero:Heal(baseHeal, nil)
 end
